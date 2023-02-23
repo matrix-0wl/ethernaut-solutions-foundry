857e04800d0949bdd818ce32b781f774332b2aab
matrix_0wl <matrix.0wl90@gmail.com>
2023-02-23 12:39:01 +0100

    Update conf file to be compatible with 0.8.0 solidity

---
+++ test/BaseTemplate-08.t.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// testing functionalities
+import "forge-std/Test.sol";
+
+// Ethernaut game components
+import {Ethernaut} from "src/core/Ethernaut-08.sol";
+import {PreservationFactory, Preservation} from "src/levels/16-Preservation/PreservationFactory.sol"; //change this @todo
+
+//change this @todo
+contract PreservationTest is
+    Test //change this @todo
+{
+    /*//////////////////////////////////////////////////////////////
+                            GAME INSTANCE SETUP
+    //////////////////////////////////////////////////////////////*/
+
+    Ethernaut ethernaut;
+
+    address attacker = makeNameForAddress("attacker");
+
+    function setUp() public {
+        emit log_string("Setting up Fallback level..."); //change this @todo
+        ethernaut = new Ethernaut();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                LEVEL INSTANCE -> EXPLOIT -> SUBMISSION
+    //////////////////////////////////////////////////////////////*/
+
+    function testFallbackExploit() public {
+        //change this @todo
+        /*//////////////////////////////////////////////////////////////
+                            LEVEL INSTANCE SETUP
+        //////////////////////////////////////////////////////////////*/
+
+        FallbackFactory fallbackFactory = new FallbackFactory(); //change this @todo
+
+        ethernaut.registerLevel(fallbackFactory); //change this @todo
+        vm.startPrank(attacker);
+
+        address levelAddress = ethernaut.createLevelInstance(fallbackFactory); //change this @todo
+        Fallback fallbackContract = Fallback(payable(levelAddress)); //change this @todo
+
+        emit log_string("Starting the exploit...");
+        emit log_named_address("Attacker's address", attacker);
+
+        /*//////////////////////////////////////////////////////////////
+                                LEVEL EXPLOIT
+        //////////////////////////////////////////////////////////////*/
+
+        // @todo
+
+        /*//////////////////////////////////////////////////////////////
+                                LEVEL SUBMISSION
+        //////////////////////////////////////////////////////////////*/
+
+        bool challengeCompleted = ethernaut.submitLevelInstance(
+            payable(levelAddress)
+        );
+        vm.stopPrank();
+        assert(challengeCompleted);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            HELPER FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * I've found this useful function
+     * in github/twpony Ethernaut repo
+     * @notice This function allows for creating labels (names) for addresses
+     * which will improve readability in traces
+     * @param name you pass the name like "alice" or "bob" and it will create
+     * an address for that person
+     */
+    function makeNameForAddress(string memory name) public returns (address) {
+        address addr = address(
+            uint160(uint256(keccak256(abi.encodePacked(name))))
+        );
+        vm.label(addr, name);
+        return addr;
+    }
+}
